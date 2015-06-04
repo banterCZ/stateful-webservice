e36b73f400152a0a8526612184fc6ff694c91500
banterCZ <lubos.racansky@gmail.com>
2015-06-04 15:17:03 +0200

    contract first

--- src/main/java/cz/zvestov/ws/CounterWSImpl.java
+++ src/main/java/cz/zvestov/ws/CounterWSImpl.java
@@ -3,16 +3,18 @@ package cz.zvestov.ws;
 import javax.inject.Inject;
 import javax.jws.WebService;
 
-@WebService
-public class CounterWS {
+@WebService(serviceName = "CounterWS", endpointInterface = "cz.zvestov.ws.CounterWS")
+public class CounterWSImpl implements CounterWS {
 
     @Inject
     private Counter counter;
 
+    @Override
     public void increase() {
         counter.increase();
     }
 
+    @Override
     public Integer getCount() {
         return counter.getCount();
     }
